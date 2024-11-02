091ba831d92f22c462b9f7043a4478c0776fd190
Ilkin Ibadov <ilkinibadov@Ilkins-MacBook-Air.local>
2024-11-02 17:58:10 +0400

    test17

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,28 @@ const App = () => {
   const [lastX, setLastX] = useState(null);
   const [lastY, setLastY] = useState(null);
   const [lastZ, setLastZ] = useState(null);
-  const [error, setError] = useState(null); // To store any error messages
-  const [permissionGiven, setPermissionGiven] = useState(false)
   const [iosDevice, setIosDevice] = useState(null)
   const shakeThreshold = 20;
+
+  function checkIfIOS13OrLater() {
+    const userAgent = navigator.userAgent || window.opera;
+
+    // Check if it is an iOS device
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      // Check for iOS version
+      const match = userAgent.match(/OS (\d+)_/); // Extract version number
+      if (match && match.length > 1) {
+        const version = parseInt(match[1], 10);
+        if (version >= 13) {
+          setIosDevice(true)
+          alert("Click button to give permission")
+        } else {
+          isPermissionSupported()
+        }
+
+      }
+    }
+  }
 
   const resetCounter = () => {
     setShakeCount(0)
@@ -37,12 +55,11 @@ const App = () => {
     }
   };
 
-  const findIfNewerThanIOS13 = async () => {
+  const isPermissionSupported = async () => {
     if (typeof (DeviceMotionEvent) !== 'undefined' && typeof (DeviceMotionEvent.requestPermission) === 'function') {
-      setIosDevice(true)
-      alert("IOS Version >13 detected", "In order for shake detection to work, please click the 'Give permission' button and accept the permission request")
-    } else {
       handlePermissionRequest()
+    } else {
+      alert("Sensors API is not supported on the device")
     }
   }
 
@@ -54,7 +71,7 @@ const App = () => {
           handleMotionEvent(event)
         })
       } else {
-        alert("Permission denied")
+        alert("Permission wasn't granted for Sensors API")
       }
     } catch (error) {
       console.error(error)
@@ -62,13 +79,12 @@ const App = () => {
   }
 
   useEffect(() => {
-    findIfNewerThanIOS13()
+    /* IOS 13+ requests us to put the permission asking action on an event like a click,
+    so we check if the device is IOS 13 or later. If it is, a button is displayed and
+    user is asked to click it. If it's not, request for permission happens automatically */
+
+    checkIfIOS13OrLater()
   }, []);
-
-  // useEffect(() => {
-  //   handleMotionEvent
-  // }, [lastX, lastY, lastZ])
-
 
   return (
     <div className='w-full h-screen bg-blue-300 flex items-center justify-center'>
@@ -77,7 +93,7 @@ const App = () => {
         <h3 className='text-8xl'>{shakeCount}</h3>
 
         {iosDevice &&
-          <button onClick={handlePermissionRequest} className='px-5 py-3 bg-blue-600 rounded-2xl text-base text-white mt-2'>Give permission</button>
+          <button onClick={isPermissionSupported} className='px-5 py-3 bg-blue-600 rounded-2xl text-base text-white mt-2'>Give permission</button>
         }
 
         <button onClick={resetCounter} className='px-5 py-3 bg-blue-600 rounded-2xl text-base text-white mt-2'>Reset Count</button>
