87c141dfa2cb08142d286a01c9f7730e976c5940
Ilkin Ibadov <ilkinibadov@Ilkins-MacBook-Air.local>
2024-11-02 15:20:23 +0400

    test3

--- src/App.jsx
+++ src/App.jsx
@@ -6,66 +6,44 @@ const App = () => {
   const [lastY, setLastY] = useState(null);
   const [lastZ, setLastZ] = useState(null);
   const [error, setError] = useState(null); // To store any error messages
-  const shakeThreshold = 1;
+  const shakeThreshold = 10;
 
   useEffect(() => {
-    // navigator.permissions.query({ name: "accelerometer" }).then((result) => {
-    //   if (result.state === "denied") {
-    //     console.log("Permission to use accelerometer sensor is denied.");
-    //     return;
-    //   }
-    //   // Use the sensor.
-    // });
+    const handleMotionEvent = (event) => {
+      const { acceleration } = event;
 
-    // let accelerometer = null;
-    // try {
-    //   accelerometer = new Accelerometer({ referenceFrame: "device" });
-    //   accelerometer.addEventListener("error", (event) => {
-    //     // Handle runtime errors.
-    //     if (event.error.name === "NotAllowedError") {
-    //       // Branch to code for requesting permission.
-    //     } else if (event.error.name === "NotReadableError") {
-    //       console.log("Cannot connect to the sensor.");
-    //     }
-    //   });
-    //   accelerometer.addEventListener("reading", () => reloadOnShake(accelerometer));
-    //   accelerometer.start();
-    // } catch (error) {
-    //   // Handle construction errors.
-    //   if (error.name === "SecurityError") {
-    //     // See the note above about permissions policy.
-    //     console.log("Sensor construction was blocked by a permissions policy.");
-    //   } else if (error.name === "ReferenceError") {
-    //     console.log("Sensor is not supported by the User Agent.");
-    //   } else {
-    //     throw error;
-    //   }
-    // }
+      if (acceleration) {
+        const { x, y, z } = acceleration;
 
-    // function handleOrientation(event) {
-    //   var absolute = event.absolute;
-    //   var alpha    = event.alpha;
-    //   var beta     = event.beta;
-    //   var gamma    = event.gamma;
-    //   setLastX(alpha)
-    //   setLastY(beta)
-    //   setLastZ(gamma)
-    //   // Do stuff with the new orientation data
-    // }
+        if (lastX !== null && lastY !== null && lastZ !== null) {
+          const deltaX = Math.abs(x - lastX);
+          const deltaY = Math.abs(y - lastY);
+          const deltaZ = Math.abs(z - lastZ);
 
-    // window.addEventListener("deviceorientation", handleOrientation, true);
+          if (deltaX > shakeThreshold || deltaY > shakeThreshold || deltaZ > shakeThreshold) {
+            setShakeCount(prevCount => prevCount + 1);
+          }
+        }
 
-    if (typeof(DeviceMotionEvent) !== 'undefined' && typeof(DeviceMotionEvent.requestPermission) === 'function')
-      {
-        setLastX("supported")
-          // iOS 13 device (supports the Permissions API)
+        setLastX(x);
+        setLastY(y);
+        setLastZ(z);
       }
-      else
-      {
-        setLastX("not supported")
+    };
 
-          // other devices
-      }
+    if (typeof (DeviceMotionEvent) !== 'undefined' && typeof (DeviceMotionEvent.requestPermission) === 'function') {
+      DeviceMotionEvent.requestPermission()
+        .then(response => {
+          if (response == "granted") {
+            window.addEventListener("devicemotion", (event) => {
+              handleMotionEvent(event)
+            })
+          }
+        })
+    }
+    else {
+      alert("not supported")
+    }
 
   }, [])
 
@@ -74,27 +52,7 @@ const App = () => {
   //   let isSensorAvailable = true;
 
   //   // Function to detect shake based on acceleration data
-  //   const handleMotionEvent = (event) => {
-  //     const { acceleration } = event;
 
-  //     if (acceleration) {
-  //       const { x, y, z } = acceleration;
-
-  //       if (lastX !== null && lastY !== null && lastZ !== null) {
-  //         const deltaX = Math.abs(x - lastX);
-  //         const deltaY = Math.abs(y - lastY);
-  //         const deltaZ = Math.abs(z - lastZ);
-
-  //         if (deltaX > shakeThreshold || deltaY > shakeThreshold || deltaZ > shakeThreshold) {
-  //           setShakeCount(prevCount => prevCount + 1);
-  //         }
-  //       }
-
-  //       setLastX(x);
-  //       setLastY(y);
-  //       setLastZ(z);
-  //     }
-  //   };
 
   //   // Check for sensor support and permissions
   //   const initializeShakeDetection = async () => {
@@ -136,7 +94,7 @@ const App = () => {
     <div className='w-full h-screen bg-blue-300 flex items-center justify-center'>
       <div className='flex flex-col items-center gap-3'>
         <h1 className='text-4xl'>Shake count:</h1>
-        <h3 className='text-8xl'>{lastX}</h3>
+        <h3 className='text-8xl'>{shakeCount}</h3>
         <button onClick={() => { setShakeCount(0) }} className='px-5 py-3 bg-blue-600 rounded-2xl text-base text-white mt-2'>Reset Count</button>
         {error ?? (
           <p style={{ color: 'red' }}>{error}</p>
